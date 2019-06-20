052441a2c589a3960772592933d2c5aa81471846
Hugo How <hugohowchoong@gmail.com>
2019-06-20 20:50:34 +0200

    refacto and clean

--- builtins/ft_env.c
+++ builtins/ft_env.c
@@ -6,22 +6,18 @@
 /*   By: hhow-cho <hhow-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/20 13:58:20 by hhow-cho          #+#    #+#             */
-/*   Updated: 2019/06/20 16:56:10 by hhow-cho         ###   ########.fr       */
+/*   Updated: 2019/06/20 20:39:51 by hhow-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
+# define FLAG_ENV_I (2 << 0)
+
 /*
 ** Builtin to get the environnement of the process
 */
 
-typedef struct s_token_env
-{
-    char *type;
-    char flag_type;
-    char *string;
-}               t_token_env;
 
 void ft_print_env(t_env **cpy_env, int fds[])
 {
@@ -47,157 +43,16 @@ t_env **clear_environ(void)
     return (output);
 }
 
-t_token_env *create_token_env(char *type, char flag_type, char *string)
-{
-    t_token_env *token_env;
 
-    if (!(token_env = ft_memalloc(sizeof(t_token_env))))
-		return (NULL);
-    token_env->type = type;
-    token_env->flag_type = flag_type;
-    token_env->string = string;   
-    return (token_env);
-}
 
-int is_option(char *str)
+
+static int ft_execute_env(char **argv, int flag, t_env **cpy_environ, int fds[])
 {
     int i;
-
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] == '=')
-            return (1);
-        i++;
-    }
-    return (0);
-}
-
-t_token_env **tokenize_argv(int argc, char **argv)
-{
-    int i;
-    int j;
-    int k;
-    t_token_env **list;
-    char *utility;
-
-    if (!(list = (t_token_env **)ft_memalloc((argc + 3) * sizeof(t_token_env *))))
-		return (NULL);
-    i = 0;
-    k = 0;
-    utility = "";
-    while (i < argc)
-    {
-        if (i == 0)
-        {
-            list[k++] = create_token_env("command", 0, argv[0]);
-        }
-        else if (argv[i][0] && argv[i][0] == '-')
-        {
-            j = 1;
-            while (argv[i][j])
-            {
-                if (argv[i][j] == 'i')
-                    list[k++] = create_token_env("flag", 'i', NULL);
-                else if (argv[i][j] == 'v')
-                    list[k++] = create_token_env("flag", 'v', NULL);
-                else if (argv[i][j] == 'P')
-                {
-                    if (argv[i + 1] && argv[i + 1][0] != '-')
-                    {
-                        list[k++] = create_token_env("flag", 'P', argv[i + 1]);
-                        i++;
-                    }
-                    else
-                    {
-                        ft_putstr_fd("Error P n'a pas de path\n", 2);
-                        exit(-1);
-                    }
-                }
-                else if (argv[i][j] == 'S')
-                {
-                    if (argv[i + 1] && argv[i + 1][0] != '-')
-                    {
-                        list[k++] = create_token_env("flag", 'S', argv[i + 1]);
-                        i++;
-                    }
-                    else
-                    {
-                        ft_putstr_fd("Error S n'a pas de path\n", 2);
-                        exit(-1);
-                    }
-                }
-                else if (argv[i][j] == 'u')
-                {
-                    if (argv[i + 1] && argv[i + 1][0] != '-')
-                    {
-                        list[k++] = create_token_env("flag", 'u', argv[i + 1]);
-                        i++;
-                    }
-                    else
-                    {
-                        ft_putstr_fd("Error u n'a pas de path\n", 2);
-                        exit(-1);
-                    }
-                }
-                else
-                {
-                    ft_putstr_fd("Illegal option !", 2);
-                    ft_putchar_fd(argv[i][j], 2);
-                    ft_putstr_fd("\n", 2);
-                    exit(-1);
-                }
-                j++;
-            }
-        }
-        else
-        {
-            while (argv[i] && is_option(argv[i]) == 1)
-            {
-                list[k++] = create_token_env("option", 0, argv[i]);
-                i++;
-            }
-            if (argv[i])
-            {
-                while (argv[i])
-                {
-                    utility = ft_strjoin(utility, " ");
-                    utility = ft_strjoin(utility, argv[i]);
-                    i++;
-                }
-                list[k++] = create_token_env("utility", 0, utility);
-            }
-        }
-
-        i++;
-    }
-    list[k] = 0;
-    return (list);
-}
-
-int has_i(t_token_env **token_ls)
-{
-    int i;
-
-    i = 0;
-    while (token_ls[i])
-    {
-        if (token_ls[i]->flag_type == 'i')
-            return (1);
-        i++;
-    }
-    return (0);
-}
-
-static int execute_ls(t_token_env **token_ls, t_env **cpy_environ, int fds[])
-{
-    int i;
-    int j;
     t_env **copy_env;
-    char *tmp;
 
     i = 0;
-    if (has_i(token_ls))
+    if (flag == FLAG_ENV_I)
         copy_env = clear_environ();
     else
         copy_env = ft_env_deep_copy(cpy_environ);
@@ -206,30 +61,31 @@ static int execute_ls(t_token_env **token_ls, t_env **cpy_environ, int fds[])
 		ft_putstr_fd("Error copy env", 2);
 		return (-1);
 	}
-    while (token_ls[i])
+    while (*argv)
     {
-        if (ft_strcmp(token_ls[i]->type, "option") == 0)
+        if (ft_strchr(*argv, '='))
         {
-            tmp = token_ls[i]->string;
-            j = 0;
-            while (tmp[j] && tmp[j] != '=')
-            {
-                j++;
-            }
-            ft_env_add(ft_strsub(tmp, 0, j), tmp + j + 1, &copy_env);
         }
-        i++;
+		else
+			break ;
+        argv++;
     }
-    if (i > 0 && ft_strcmp(token_ls[i - 1]->type, "utility") == 0)
+    if (*argv)
 	{
 		t_node **root;
 		int success;
-		int fds[3];
 
-		fds[0] = 0;
-		fds[1] = 1;
-		fds[2] = 2;
-		root = ft_syntax_tree_create(token_ls[i - 1]->string, cpy_environ);
+		char *cmd;
+
+		cmd = ft_memalloc(999 * sizeof(char));
+		while (*argv)
+		{
+			cmd = ft_strcat(cmd, *argv);
+			cmd = ft_strcat(cmd, " ");
+			argv++;
+		}
+
+		root = ft_syntax_tree_create(cmd, cpy_environ);
 		success = 0;
     	execute_tree(*root, &copy_env, fds, &success);
 		return (success);
@@ -239,39 +95,62 @@ static int execute_ls(t_token_env **token_ls, t_env **cpy_environ, int fds[])
     return (0);
 }
 
-void print_token_ls(t_token_env **token_ls)
-{
-    int i;
 
-    i = 0;
-    while (token_ls[i] != 0)
-    {
-        // ft_printf("type : %ld \t flag: %c \t string : %s\n", token_ls[i]->type, token_ls[i]->flag_type, token_ls[i]->string);
-        i++;
-    }
+static int ft_parse_input_env(char ***p_argv, int fds[])
+{
+	int k;
+	int i;
+	int flag;
+
+	k = 0;
+	flag = 0;
+	while ((*p_argv)[k])
+	{
+		i = 1;
+		if ((*p_argv)[k][0] != '-')
+			break ;
+		while ((*p_argv)[k][i])
+		{
+			if ((*p_argv)[k][i] == 'i')
+				flag = FLAG_ENV_I;
+			else
+			{
+				ft_dprintf(fds[2], "env: illegal option -- %c\n", (*p_argv)[k][i]);
+				ft_putstr_fd("usage: env [-i] [name=value ...] [utility [argument ...]]\n", fds[2]);
+				return (-1);
+			}
+				break ;
+			i++;
+		}
+		*p_argv = *p_argv + 1;
+		k++;
+	}
+	return (flag);
 }
 
 int ft_env(int argc, char **argv, t_env **cpy_environ, int fds[])
 {
     pid_t pid;
     int status;
-    t_token_env **token_ls;
     int waitstatus;
     int i;
+	int flag;
 
     if (argc == 1)
     {
         ft_print_env(cpy_environ, fds);
         return (0);
     }
+	flag = 0;
+	argv++;
+	flag = ft_parse_input_env(&argv, fds);
+	if (flag < 0)
+		return (1);
     status = 0;
     pid = fork();
     if (pid == 0)
     {
-        token_ls = tokenize_argv(argc, argv);
-        // permet de vérfier si il y a des anomalies
-        print_token_ls(token_ls);
-        status = execute_ls(token_ls, cpy_environ, fds);
+        status = ft_execute_env(argv, flag, cpy_environ, fds);
         exit(status);
     }
     else if (pid < 0)
