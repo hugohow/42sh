cc2d017c07184beee6d2240d973d0f14b1c09464
Hugo How <hugohowchoong@gmail.com>
2019-06-23 00:52:30 +0200

    tests

--- back/utils/ft_bins_table_create.c
+++ back/utils/ft_bins_table_create.c
@@ -6,7 +6,7 @@
 /*   By: hhow-cho <hhow-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/09 15:24:14 by hhow-cho          #+#    #+#             */
-/*   Updated: 2019/06/23 00:50:04 by hhow-cho         ###   ########.fr       */
+/*   Updated: 2019/06/23 00:52:26 by hhow-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@ t_ht *ft_bins_table_create(char *line)
 		paths = ft_strsplit("/usr/sbin:/usr/bin:/sbin:/bin:.", ':');
 	else
 		paths = ft_strsplit(line + 5, ':');
-	printf("paths :: %s\n", paths);
 	i = 0;
     while (paths[i])
     {
+		printf("paths[i] :: %s\n", paths[i);
 		if ((pDir = opendir (paths[i])) == NULL)
 		{
 			break;
