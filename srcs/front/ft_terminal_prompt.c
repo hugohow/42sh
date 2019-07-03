22fe01be38f9782abca7c1b2d17cb965126c6309
Hugo How <hugohowchoong@gmail.com>
2019-07-03 22:16:13 +0200

    solve terminal prompt issue

--- srcs/front/ft_terminal_prompt.c
+++ srcs/front/ft_terminal_prompt.c
@@ -6,7 +6,7 @@
 /*   By: hhow-cho <hhow-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 23:13:26 by hhow-cho          #+#    #+#             */
-/*   Updated: 2019/07/03 17:14:45 by hhow-cho         ###   ########.fr       */
+/*   Updated: 2019/07/03 22:15:36 by hhow-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void ft_terminal_prompt(void)
 {
 	char *cwd;
 
-	cwd = (char *)ft_vars_get_value(KEY_CWD);
+	cwd = *((char **)ft_vars_get_value(KEY_CWD));
 	ft_putchar_fd('\r', 0);
 	ft_putstr_fd(NAME, 0);
 	ft_putstr_fd(ft_strrchr(cwd, '/'), 0);
