bda5fbffd4de17915dadcee58e336fce92146316
Hugo HOW-CHOONG <hhow-cho@e1r2p9.42.fr>
2019-07-07 16:10:40 +0200

    add exeption zhen execute a folder

--- srcs/back/utils/ft_bin_is_accessible.c
+++ srcs/back/utils/ft_bin_is_accessible.c
@@ -6,7 +6,7 @@
 /*   By: hhow-cho <hhow-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/06 19:41:33 by hhow-cho          #+#    #+#             */
-/*   Updated: 2019/07/06 19:41:51 by hhow-cho         ###   ########.fr       */
+/*   Updated: 2019/07/07 16:08:42 by hhow-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,21 @@ int ft_bin_is_accessible(char *path, int fds[])
 		lstat(path, &fileStat);
 		if (S_ISLNK(fileStat.st_mode) && access(path, X_OK))
 		{
-			ft_dprintf(fds[2], "Too many symbolic links: %s \n", path);
+			ft_dprintf(fds[2], "minishell: Too many symbolic links: %s \n", path);
 			return (EXIT_FAIL);
 		}
-		ft_dprintf(fds[2], "No such file or directory: %s \n", path);
+		ft_dprintf(fds[2], "minishell: No such file or directory: %s \n", path);
 		return (EXIT_FAIL);
 	}
 	else if (access(path, X_OK))
 	{
-		ft_dprintf(fds[2], "permission denied : %s \n", path);
+		ft_dprintf(fds[2], "minishell: permission denied : %s \n", path);
 		return (EXIT_FAIL);
+	}
+	else if (S_ISDIR(fileStat.st_mode))
+	{
+		ft_dprintf(fds[2], "minishell: %s is not an executable\n", path);
+		return (EXIT_UTILITY_NOT_EXEC);
 	}
 	return (0);
 }
