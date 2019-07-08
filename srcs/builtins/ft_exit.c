191db02bdd67aabb999bf980b9f940c76add0a2f
Hugo HOW-CHOONG <hhow-cho@e1r3p3.42.fr>
2019-07-08 20:29:50 +0200

    add tests for exit

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hhow-cho <hhow-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/20 13:58:14 by hhow-cho          #+#    #+#             */
-/*   Updated: 2019/07/04 19:55:23 by hhow-cho         ###   ########.fr       */
+/*   Created: 2019/07/08 20:11:35 by hhow-cho          #+#    #+#             */
+/*   Updated: 2019/07/08 20:27:44 by hhow-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,20 @@
 int ft_exit(char **argv, t_env ***p_cpy_environ, int fds[])
 {
     int i;
+	char *line;
 
-	(void)p_cpy_environ;
-	if (argv[1] && argv[2])
-	{
-		ft_putstr_fd("shell: exit: too many arguments\n", fds[2]);
-		return (EXIT_FAIL);
-	}
-	*((int *)ft_vars_get_value(KEY_MUST_EXIT)) = 1;
     if (argv[1])
     {
         i = 0;
         while (argv[1][i] && argv[1][i] == ' ')
-            i++;
+			i++;
         while (argv[1][i])
         {
             if (ft_isdigit(argv[1][i]) == 0)
             {
                 ft_putstr_fd("shell: Numeric argument required\n", fds[2]);
-                return (2);
+				*((int *)ft_vars_get_value(KEY_MUST_EXIT)) = 1;
+                return (255);
             }
             i++;
         }
@@ -46,7 +41,12 @@ int ft_exit(char **argv, t_env ***p_cpy_environ, int fds[])
             ft_putstr_fd("shell: exit: too many arguments\n", fds[2]);
             return (EXIT_FAIL);
         }
+		*((int *)ft_vars_get_value(KEY_MUST_EXIT)) = 1;
         return (ft_atoi(argv[1]));
     }
+	line = ft_env_get_value(*p_cpy_environ, "?");
+	*((int *)ft_vars_get_value(KEY_MUST_EXIT)) = 1;
+	if (line)
+		return (ft_atoi(line));
 	return (*((int *)ft_vars_get_value(KEY_SUCCESS_EXIT)));
 }
