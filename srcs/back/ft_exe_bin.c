4dec5e91ae4aeb690279fe63d0f9e483b0639311
Hugo HOW-CHOONG <hhow-cho@e1r3p3.42.fr>
2019-07-08 18:30:10 +0200

    refacto and norm

--- srcs/back/ft_exe_bin.c
+++ srcs/back/ft_exe_bin.c
@@ -6,7 +6,7 @@
 /*   By: hhow-cho <hhow-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/09 01:40:14 by hhow-cho          #+#    #+#             */
-/*   Updated: 2019/07/08 18:17:55 by hhow-cho         ###   ########.fr       */
+/*   Updated: 2019/07/08 18:29:06 by hhow-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ static int	ft_search_in_curr_dir(char **args, t_env **cpy_environ, int fds[])
 	DIR				*p_dir;
 	struct dirent	*p_dirent;
 	char			*cwd;
-	char *to_free;
+	char			*to_free;
 
 	if ((p_dir = opendir(".")))
 	{
@@ -52,10 +52,10 @@ static int	ft_search_in_curr_dir(char **args, t_env **cpy_environ, int fds[])
 static int	ft_search_and_exe_bin(char **args, t_env **cpy_environ,\
 	t_ht **p_table_bins, int fds[])
 {
-	int result_cmd;
-	char *command;
-	t_node_ht *value;
-	char *to_free;
+	int			result_cmd;
+	char		*command;
+	t_node_ht	*value;
+	char		*to_free;
 
 	command = args[0];
 	if (p_table_bins \
@@ -73,38 +73,42 @@ static int	ft_search_and_exe_bin(char **args, t_env **cpy_environ,\
 	return (result_cmd);
 }
 
-
-
-int			ft_exe_bin(t_node *node, t_env ***p_environ, t_ht **p_table_bins, int fds[])
+static int	ft_init_args(t_node *node, t_env ***p_environ, \
+	int *p_res_parse, int *p_result_cmd)
 {
-	int		result_parsing;
-	int		result_cmd;
-	char	*new_path;
-
-	result_parsing = 0;
-	node->args = ft_args_get(node->cmd, *p_environ, &result_parsing);
+	*p_res_parse = 0;
+	*p_result_cmd = 0;
+	node->args = ft_args_get(node->cmd, *p_environ, p_res_parse);
 	if (node->args == NULL || node->args[0] == NULL)
 	{
 		ft_list_free(&(node->args));
 		return (1);
 	}
-	new_path = NULL;
-	result_cmd = 0;
+	return (0);
+}
+
+int			ft_exe_bin(t_node *node, t_env ***p_env, t_ht **p_hash, int fds[])
+{
+	int		res_parse;
+	int		result_cmd;
+
+	if (ft_init_args(node, p_env, &res_parse, &result_cmd) == 1)
+		return (1);
 	if (ft_is_path(node->args[0]) == 1)
-		result_cmd = ft_exe_path(node->args, *p_environ, fds);
+		result_cmd = ft_exe_path(node->args, *p_env, fds);
 	else if (ft_strcmp(node->args[0], BUILTIN_EXIT) == 0)
-		result_cmd = ft_exit(node->args, p_environ, fds);
+		result_cmd = ft_exit(node->args, p_env, fds);
 	else if (ft_strcmp(node->args[0], BUILTIN_ECHO) == 0)
-		result_cmd = ft_echo(node->args, *p_environ, fds);
+		result_cmd = ft_echo(node->args, *p_env, fds);
 	else if (ft_strcmp(node->args[0], BUILTIN_CD) == 0)
-		result_cmd = ft_cd(node->args, p_environ, fds);
+		result_cmd = ft_cd(node->args, p_env, fds);
 	else if (ft_strcmp(node->args[0], BUILTIN_SETENV) == 0)
-		result_cmd = ft_setenv(node->args, p_environ, p_table_bins, fds);
+		result_cmd = ft_setenv(node->args, p_env, p_hash, fds);
 	else if (ft_strcmp(node->args[0], BUILTIN_UNSETENV) == 0)
-		result_cmd = ft_unsetenv(node->args, *p_environ, p_table_bins, fds);
+		result_cmd = ft_unsetenv(node->args, *p_env, p_hash, fds);
 	else if (ft_strcmp(node->args[0], BUILTIN_ENV) == 0)
-		result_cmd = ft_env(node->args, *p_environ, fds);
+		result_cmd = ft_env(node->args, *p_env, fds);
 	else
-		result_cmd = ft_search_and_exe_bin(node->args, *p_environ, p_table_bins, fds);
-	return (ft_max(result_cmd, result_parsing));
+		result_cmd = ft_search_and_exe_bin(node->args, *p_env, p_hash, fds);
+	return (ft_max(result_cmd, res_parse));
 }
