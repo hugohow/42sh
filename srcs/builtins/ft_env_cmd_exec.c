1af4a1f159138e3676bcdb7332e4f5abcb86b6ab
Hugo HOW-CHOONG <hhow-cho@e1r1p5.42.fr>
2019-07-13 21:07:06 +0200

    pre push to prod

--- srcs/builtins/ft_env_cmd_exec.c
+++ srcs/builtins/ft_env_cmd_exec.c
@@ -6,7 +6,7 @@
 /*   By: hhow-cho <hhow-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/13 13:33:15 by hhow-cho          #+#    #+#             */
-/*   Updated: 2019/07/13 21:01:56 by hhow-cho         ###   ########.fr       */
+/*   Updated: 2019/07/13 21:04:59 by hhow-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ static int	ft_env_search_in_hash(char **argv, \
 {
 	t_ht		*hash;
 	t_node_ht	*value;
+	char		*to_free;
 
 	if (*p_hash)
 		hash = *p_hash;
@@ -29,8 +30,7 @@ static int	ft_env_search_in_hash(char **argv, \
 		to_free = (argv)[0];
 		(argv)[0] = ft_strdup((char *)(value->datum));
 		ft_memdel((void **)&to_free);
-		result_cmd = ft_exe_path((argv), *p_copy_env, fds);
-		return (result_cmd);
+		return (ft_exe_path((argv), *p_copy_env, fds));
 	}
 	ft_dprintf(fds[2], "minishell env: command not found %s\n", argv[0]);
 	return (EXIT_UTILITY_NOT_FOUND);
@@ -40,18 +40,12 @@ int			ft_env_cmd_exec(char **argv, \
 	t_ht **p_hash, t_env ***p_copy_env, int fds[])
 {
 	int		i;
-	int		result_cmd;
-	t_ht	*hash;
-	char	*to_free;
 
 	i = 0;
 	if (*argv)
 	{
 		if (ft_is_path(*argv) == 1)
-		{
-			result_cmd = ft_exe_path(argv, *p_copy_env, fds);
-			return (result_cmd);
-		}
+			return (ft_exe_path(argv, *p_copy_env, fds));
 		else
 			return (ft_env_search_in_hash(argv, p_hash, p_copy_env, fds));
 	}
