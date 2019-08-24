e9211aa4285690464fd90cd1343d01a88f55e4ff
Keny SAINT HILAIRE <kesaint-@e1r6p19.42.fr>
2019-08-24 17:47:20 +0200

    :construction: fix ft_quote (cursor in progress)

--- includes/closures.h
+++ includes/closures.h
@@ -1,10 +1,7 @@
 #ifndef CLOSURES_H
 # define CLOSURES_H
-
+// File to delete later
 # include "shell.h"
 
 int			check_closures(t_list *list, char *closures);
-void		on_enter_key(t_cmd *cmd);
-void		on_key_press(t_cmd *cmd);
-
 #endif
